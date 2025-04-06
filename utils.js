e66587fb5e814249b848dd9c063aaea06fd1efac
AndreasB09 <andreasb09.kodehode@gmail.com>
2025-04-06 18:38:26 +0200

    fixed function name

--- utils.js
+++ utils.js
@@ -4,10 +4,10 @@ function setupRulesLinks() {
     const ruleLinks = document.querySelectorAll(".rule-link");
     const ruleTooltip = document.getElementById("rule-tooltip");
 
-    tooltip.addEventListener("mouseleave", () => {
-        tooltip.style.display = "none";
+    ruleTooltip.addEventListener("mouseleave", () => {
+        ruleTooltip.style.display = "none";
     });
-    
+
     ruleLinks.forEach(link => {
         link.addEventListener("mouseenter", async (e) => {
             const ruleId = e.target.dataset.rule;
